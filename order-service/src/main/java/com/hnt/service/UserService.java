e271e4f12f4732d1234721d787a6df1476db7c32
unknown <cogjava3169@iiht.tech>
2022-08-23 10:12:15 +0530

    Day1

---
+++ order-service/src/main/java/com/hnt/service/UserService.java
@@ -0,0 +1,21 @@
+package com.hnt.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hnt.UserRepository;
+import com.hnt.entity.User;
+
+@Service
+public class UserService {
+@Autowired
+UserRepository repository;
+	public void save(User user) {
+		// TODO Auto-generated method stub
+		repository.save(user);
+		
+	}
+
+
+	
+}
