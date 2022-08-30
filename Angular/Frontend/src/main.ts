f302f2702973e0ac19b18f9f4a4c06cfca9dc4c2
unknown <cogjava3169@iiht.tech>
2022-08-30 11:25:10 +0530

    Angular

--- Angular/Frontend/src/main.ts
+++ Angular/Frontend/src/main.ts
@@ -1,4 +1,7 @@
+//main.ts is for bootstrapping the root module.
+
 import { enableProdMode } from '@angular/core';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,5 +11,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)//Appmodule is root module.module has components.
+  .catch(err => console.error(err));//in case of failures or missing modules
